0a56a29bf83b2f8a6b9aed4a56b097e086bbcd7a
1l0 <rh.plus001@gmail.com>
2025-11-18 01:09:27 +0900

    embed schema

--- schema.sql
+++ schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS events (
 CREATE INDEX IF NOT EXISTS time_idx ON events(created_at DESC, id ASC);
 CREATE INDEX IF NOT EXISTS kind_sorted_idx ON events(kind, created_at DESC, id ASC);
 CREATE INDEX IF NOT EXISTS pubkey_kind_sorted_idx ON events(pubkey, kind, created_at DESC, id ASC);
-	
+
 CREATE TABLE IF NOT EXISTS tags (
     event_id TEXT NOT NULL,
     key TEXT NOT NULL,
@@ -26,7 +26,7 @@ CREATE INDEX IF NOT EXISTS tags_event_id_idx ON tags(event_id);
 CREATE TRIGGER IF NOT EXISTS d_tags_ai AFTER INSERT ON events
 WHEN NEW.kind BETWEEN 30000 AND 39999 
 BEGIN
-INSERT OR IGNORE INTO tags (event_id, key, value)
+INSERT INTO tags (event_id, key, value)
     SELECT NEW.id, 'd', json_extract(value, '$[1]')
     FROM json_each(NEW.tags)
     WHERE json_type(value) = 'array' AND json_array_length(value) > 1 AND json_extract(value, '$[0]') = 'd'
